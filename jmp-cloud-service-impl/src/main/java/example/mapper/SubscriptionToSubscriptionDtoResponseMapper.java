8fd2aefb3ef170f6c16732397ce87f9347355ecd
Vasili_Balyka <Vasili_Balyka@epam.com>
2023-03-21 16:53:56 +0400

    feat | REST-basics | fixed build

--- jmp-cloud-service-impl/src/main/java/example/mapper/SubscriptionToSubscriptionDtoResponseMapper.java
+++ jmp-cloud-service-impl/src/main/java/example/mapper/SubscriptionToSubscriptionDtoResponseMapper.java
@@ -5,6 +5,7 @@ import com.epam.example.dto.SubscriptionResponseDto;
 import java.util.List;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",
@@ -14,6 +15,7 @@ public interface SubscriptionToSubscriptionDtoResponseMapper {
 
   List<SubscriptionResponseDto> map(List<Subscription> source);
 
+  @Mapping(target = "userId", source = "user.id")
   SubscriptionResponseDto map(Subscription source);
 
 }
